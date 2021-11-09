41a1f5f63d5935f13da90eca158aee300ce2f733
Kendrick2000 <s3814172@rmit.edu.vn>
2021-11-09 14:52:04 +0700

    first commit

---
+++ assign1-students/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Student Management System</header>
+      <h1></h1>
+    </div>
+  );
+}
+
+export default App;
