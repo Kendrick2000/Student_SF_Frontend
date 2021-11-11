8df3c7a31fac9735a706927311711125207da7c7
Kendrick2000 <s3814172@rmit.edu.vn>
2021-11-11 21:06:46 +0700

    Update App.js

--- assign1-students/src/App.js
+++ assign1-students/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
-import AddNewStudent from './AddNewStudent';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AddNewStudent from "./AddNewStudent";
 
 const STUDENT_API_URL = 'http://localhost:8080/api/a1/student';
 
@@ -21,49 +21,45 @@ const StudentProfiles = () => {
   }, []);
 
   return (
-    <div className="container">
-      <Router>
-        <Link to="/AddNewStudent" >Add Student</Link>
-        <Switch>
-          <Route exact path='/AddNewStudent' component={AddNewStudent}></Route>
-        </Switch>    
-      </Router>
-      <h1>List of Sutdent</h1>
-      <hr/>
-      <div>
-        {/* <Link to="/AddNewStudent" className="btn btn-primary mb-2">Add Student</Link> */}
-        {/* <Link to="/AddNewStudent" >Add Student</Link>
-        <Route path="/AddNewStudent">
-          <AddNewStudent />
-        </Route> */}
-        <table className="table table-border">
-          <thead>
-            <tr>
-              <td>ID</td>
-              <td>First Name</td>
-              <td>Last Name</td>
-              <td>Address</td>
-              <td>Phone Number</td>
-              <td>Email</td>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              studentProfiles.map(studentProfiles => (
-                <tr key={studentProfiles.id}>
-                  <td>{studentProfiles.id}</td>
-                  <td>{studentProfiles.name}</td>
-                  <td>{studentProfiles.parentName}</td>
-                  <td>{studentProfiles.address}</td>
-                  <td>{studentProfiles.phoneNum}</td>
-                  <td>{studentProfiles.email}</td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
+    <Router>
+      <div className="container">
+        <h1>List of Sutdent</h1>
+        <hr/>
+        <div>
+          <table className="table table-border">
+            <thead>
+              <tr>
+                <td>ID</td>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Address</td>
+                <td>Phone Number</td>
+                <td>Email</td>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                studentProfiles.map(studentProfiles => (
+                  <tr key={studentProfiles.id}>
+                    <td>{studentProfiles.id}</td>
+                    <td>{studentProfiles.name}</td>
+                    <td>{studentProfiles.parentName}</td>
+                    <td>{studentProfiles.address}</td>
+                    <td>{studentProfiles.phoneNum}</td>
+                    <td>{studentProfiles.email}</td>
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+          <div>
+            <Routes>
+              <Route exact path='/AddNewStudent' component={AddNewStudent}/>
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 };
 
